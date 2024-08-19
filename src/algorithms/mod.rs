a42292f2b2ada937719df8e6370a0ecf119c1931
Nathaniel D. Hoffman <dene@cmu.edu>
2024-08-18 22:17:00 -0400

    feat: add preliminary implementation of BFGS algorithm

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -1,6 +1,9 @@
 /// Module containing the Gradient Descent minimization algorithm
 pub mod gradient_descent;
 pub use gradient_descent::{GradientDescent, GradientDescentOptions};
+/// Module containing the BFGS minimization algorithm and variants
+pub mod bfgs;
+// pub use bfgs::{BFGS, BFGSOptions};
 /// Module containing the Newton-Raphson minimization algorithm
 pub mod newton;
 pub use newton::{Newton, NewtonOptions};
